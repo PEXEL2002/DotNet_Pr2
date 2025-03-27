16854e5705502208fd29a7af2d433193cf3e47d8
Bartłomiej Kuk <bartek.kuk@o2.pl>
2025-03-27 16:19:04 +0100

    Dodano klasę do pobierania danych z API.

---
+++ DownlowdDataFromAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Pr2
+{
+    class DownlowdDataFromAPI
+    {
+        public HttpClient client;
+        public async Task<string> GetData(string call)
+        {
+            client = new HttpClient();
+            string response = await client.GetStringAsync(call);
+            return response;
+        }
+    }
+}

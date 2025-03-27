10f4a2e7e72f4281dafa6503594ad5177b65b4f1
Bartłomiej Kuk <bartek.kuk@o2.pl>
2025-03-27 20:14:34 +0100

    Dodano pobieranie danych z API i deserializację
    Wprowadzono nowe klasy `ToDoClass`, `ToDoResponse`, `UserClass` i `UserResponse` w celu reprezentacji zadań i użytkowników. Zaktualizowano `Program.cs`, aby pobierał dane z API oraz deserializował odpowiedzi JSON, wyświetlając je na konsoli. Realizacja zadania 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace DotNet_Pr2
+﻿using System.Text.Json;
+
+namespace DotNet_Pr2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DownlowdDataFromAPI dAPI = new DownlowdDataFromAPI();
+            string userJson =  dAPI.GetData("https://dummyjson.com/users").Result;
+            string todosJson = dAPI.GetData("https://dummyjson.com/todos").Result;
+            Console.WriteLine("======= Users =======");
+            var userDeserialize  = JsonSerializer.Deserialize<UserResponse>(userJson);
+            Console.WriteLine(userDeserialize.ToString());
+            Console.WriteLine("======= ToDos =======");
+            var todosDeserialize = JsonSerializer.Deserialize<ToDoResponse>(todosJson);
+            Console.WriteLine(todosDeserialize.ToString());
         }
     }
 }
